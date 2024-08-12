81dc1559a4b07e5def9678f10360dfbea2d2c18b
Choc1015 <98998206+Choc1015@users.noreply.github.com>
2024-08-12 22:28:08 +0900

    END ANIM

---
+++ Assets/Scripts/Controllers/PlayerController.cs
@@ -0,0 +1,94 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+
+public class PlayerController : MonoBehaviour
+{
+    [SerializeField]
+    float _speed = 10f;
+
+    Vector3 _destPos;
+
+    void Start()
+    {
+        Managers.Input.MouseAction -= OnMouseClicked;
+        Managers.Input.MouseAction += OnMouseClicked;
+    }
+
+    public enum PlayerState
+    {
+        Die,
+        Moving,
+        Idle,
+    }
+
+    PlayerState _state = PlayerState.Idle;
+
+    void UpdateDie()
+    {
+
+    }
+
+    void UpdateMoving()
+    {
+        Vector3 dir = _destPos - transform.position;
+        if (dir.magnitude < 0.0001f)
+        {
+            _state = PlayerState.Idle;
+        }
+        else
+        {
+            float moveDist = Mathf.Clamp(_speed * Time.deltaTime, 0, dir.magnitude);
+            transform.position += dir.normalized * moveDist;
+
+            transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.LookRotation(dir), 20 * Time.deltaTime);
+        }
+
+        // 애니메이션
+        Animator anim = GetComponent<Animator>();
+        // 현재 게임 상태에 대한 정보를 넘겨준다. 
+        anim.SetFloat("speed", _speed);
+    }
+
+    void UpdateIdle()
+    {
+        // 애니메이션
+        Animator anim = GetComponent<Animator>();
+        anim.SetFloat("speed", 0);
+    }
+
+    void Update()
+    {
+
+        switch (_state)
+        {
+            case PlayerState.Idle:
+                UpdateIdle();
+                break;
+            case PlayerState.Moving:
+                UpdateMoving();
+                break;
+            case PlayerState.Die:
+                UpdateDie();
+                break;
+        }
+    }
+
+    public void OnMouseClicked(Define.MouseEvent evt)
+    {
+        if (_state == PlayerState.Die)
+            return;
+
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        Debug.DrawRay(Camera.main.transform.position, ray.direction * 100f, Color.red, 1f);
+
+        RaycastHit hit;
+        if (Physics.Raycast(ray, out hit, 100f, LayerMask.GetMask("Wall")))
+        {
+            _destPos = hit.point;
+            _state = PlayerState.Moving;
+        }
+    }
+}
